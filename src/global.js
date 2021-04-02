87a288bbf38ebf413f49f1eb5347a4443b857705
dingning <2667752905@qq.com>
2021-04-02 16:18:37 +0800

    feat: building to update

--- src/global.js
+++ src/global.js
@@ -131,7 +131,8 @@ export const find_building = function (creep) {
             creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
         }
     } else {
-    moveto_Target(creep)
+        const controller = creep.room.controller
+        if (creep.upgradeController(controller) == ERR_NOT_IN_RANGE) creep.moveTo(controller)
     }
 }
 
