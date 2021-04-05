ab1d098b31ccb69764fa81a98e33d14ec78035a4
vouis <16nding@gmail.com>
2021-04-05 15:40:00 +0800

    north room

--- src/role/role.NorthRoomRepair.js
+++ src/role/role.NorthRoomRepair.js
@@ -11,10 +11,17 @@ import {
 const northRoomRepair = () => ({
     source: creep => {
         const room = Game.rooms['E2S34']
-        if (!room) {
-            creep.moveTo(new RoomPosition(25, 22, 'E2S34'))
+        if (room && room.find(FIND_HOSTILE_CREEPS) === true) { // 遇到invader计时，往出生点跑
+            if (Memory.invader.northRoom + decayTime < Game.time) { //不在侵略时间段，记录开始时间
+                Memory.invader.northRoom = Game.time;
+            }
+            creep.moveTo(new RoomPosition(9, 2, 'E2S35'))
+        } else if (Memory.invader.northRoom + decayTime > Game.time) { //在侵略时间段
+            creep.moveTo(new RoomPosition(9, 2, 'E2S35'))
         }
-        else {
+        else if (!room && Memory.invader.northRoom + decayTime < Game.time) { //没视野，不被侵略
+            creep.moveTo(new RoomPosition(20, 36, 'E2S34'))
+        } else if (Memory.invader.northRoom + decayTime < Game.time) { //当前不在侵略时间段
             find_structure_or_source(creep, source_North, container_North)
         }
     },
