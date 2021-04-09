c4ab79eee0b9e7be92e64c299b3bbfc08bd3892b
dingning <2667752905@qq.com>
2021-04-09 15:38:07 +0800

    feat: link upgrader

--- src/role/role.upgrader.js
+++ src/role/role.upgrader.js
@@ -1,8 +1,13 @@
-import { container_2, find_structure_or_source, source_2, storageId } from "../global";
+import { container_2, find_structure_or_source, source_2, storageId,linkUpgraderId,find_source } from "../global";
 
 const roleUpgrader = () => ({
     source: creep => {
-        find_structure_or_source(creep, source_2, container_2, storageId)
+        const linkUpgrader = Game.getObjectById(linkUpgraderId)
+        if(linkUpgrader&&linkUpgrader.energy>0){
+            find_source(creep,linkUpgraderId);
+        }else{
+            find_structure_or_source(creep, source_2, container_2, storageId)
+        }
     },
     target: creep => {
         const controller = creep.room.controller
