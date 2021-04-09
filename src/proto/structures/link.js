7ead37d15e82a48674a41baed6d3eee8866d3367
dingning <2667752905@qq.com>
2021-04-09 15:07:54 +0800

    fix

--- src/proto/structures/link.js
+++ src/proto/structures/link.js
@@ -5,12 +5,12 @@ StructureLink.prototype.work = function(){
 
     if (this.store.getUsedCapacity(RESOURCE_ENERGY) < 700) return
     if(!this.room.memory.sourceLink2Id){
-        this.room.memory.Link2Id='606bce9496af2a2cda7c90cf'
+        this.room.memory.sourceLink2Id='606bce9496af2a2cda7c90cf'
     }
 
     // 发送给 upgrader 和center
     if (this.room.memory.sourceLink2Id&& this.room.memory.sourceLink2Id === this.id) {
-        const link = Game.getObjectById(this.room.memory.Link2Id)
+        const link = Game.getObjectById(this.room.memory.sourceLink2Id)
         if(link.cooldown===0){
             link.transferEnergy(Game.getObjectById(linkCenter), link.energy);
         }
